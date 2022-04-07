41b18389819211f10c0824f1be9dd93c04c459e6
jonuvil <jonuvil@gmail.com>
2022-04-07 13:00:50 +0200

    Modificación 1

--- Bisiesto/src/Hola_Mundo.java
+++ Bisiesto/src/Hola_Mundo.java
@@ -3,5 +3,6 @@
 public class Hola_Mundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo, Soy Mateo");
+        System.out.println("Holaa Soy Jose");
     }
 }
