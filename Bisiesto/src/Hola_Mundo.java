2050f8226d349283a6c2024a99a2b60e1746e66b
jonuvil <jonuvil@gmail.com>
2022-04-20 12:49:10 +0200

    Añado println con mi nombre.Existencia de los dos nombres a la vez

--- Bisiesto/src/Hola_Mundo.java
+++ Bisiesto/src/Hola_Mundo.java
@@ -3,5 +3,7 @@
 public class Hola_Mundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo Soy Mateo");
+        System.out.println("Hola mundo Soy Jose");
+
     }
 }
