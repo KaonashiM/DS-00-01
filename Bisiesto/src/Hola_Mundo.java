ee340c46ce5f97b47d8e466a4f71332adbd9aa05
matsvei.alizar <mateo.alizar.tic@gmail.com>
2022-04-20 12:42:28 +0200

    Vuelvo a poner el hola mundo con mi nombre

--- Bisiesto/src/Hola_Mundo.java
+++ Bisiesto/src/Hola_Mundo.java
@@ -2,6 +2,6 @@
 
 public class Hola_Mundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo,Soy Jose");
+        System.out.println("Hola mundo Soy Mateo");
     }
 }
